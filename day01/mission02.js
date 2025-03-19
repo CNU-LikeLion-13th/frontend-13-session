5b539c81b5bf4d3694eb0f8bc8ed69a9a0a15928
als8921 <als8921@naver.com>
2025-03-19 21:40:43 +0900

    mission02

---
+++ day01/mission02.js
@@ -0,0 +1,13 @@
+/*
+### 미션2. 자료형 출력하기
+
+숫자형, 문자형, undefined 형 변수를 각각 선언한 다음, `typeof` 연산자를 사용해서 변수들의 자료형을 출력해주세요.
+*/
+
+var a = 10;
+var b = "abc";
+var c = undefined;
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
