8f2268a238796b35460bb82016ede3cdfe5d3b68
minhee-kim1024 <oliviathree@naver.com>
2025-03-19 22:19:23 +0900

    과제 완료

---
+++ day01/mission02.js
@@ -0,0 +1,6 @@
+let a=1;
+let b="1";
+let i;
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof i);
---
+++ day01/mission05.js
@@ -0,0 +1,8 @@
+let numbers = [3, 7, 2, 8, 5];
+let sum = 0;
+let i=0;
+while(i<numbers.length){
+    sum+=numbers[i];
+    i+=1;
+}
+console.log(sum);
