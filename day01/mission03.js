c7f29c94c798c466c36f6ad7b59c8853932dc417
dnxo1224 <dnxo1224@naver.com>
2025-03-19 22:02:51 +0900

    update: 3

---
+++ day01/mission03.js
@@ -0,0 +1,16 @@
+let animal = "cat";
+
+switch (animal) {
+  case "cat":
+    console.log("고양이");
+    break;
+  case "dog":
+    console.log("개");
+    break;
+  case "tiger":
+    console.log("개");
+    break;
+  case "lion":
+    console.log("개");
+    break;
+}
