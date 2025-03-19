c409b9f24753d846cec791836195829bbf53ffc3
sslynn22 <sslynn022@gmail.com>
2025-03-19 22:06:53 +0900

    feat: mission05

--- day01/mission04.js
+++ day01/mission04.js
@@ -3,5 +3,5 @@ let fruits = ["Apple", "Orange", "Banana", "Grapes"];
 if (fruits.find(element => element === "Banana")) {
     console.log("Banana가 있습니다.")
 } else {
-    console.log("no")
+    console.log("Banana가 없습니다.")
 }
