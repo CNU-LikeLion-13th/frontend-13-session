dc7c8f07cc4d7ec86f06b7ec99d11a00b397db70
dyw0409 <dy_woo0409@naver.com>
2025-03-20 11:42:30 +0900

    js 3 수정

--- day01/mission03.js
+++ day01/mission03.js
@@ -1,8 +1,18 @@
 
 //03
-let animal ='';
+let animal ='고양이';
+
 switch(animal){
     case '고양이':
         confirm.log("고양이")
+        break;  
+    case '강아지':
+        console.log("강아지")
+        break;  
+    case '호랑이':
+        console.log("호랑이")
+        break;  
+    case '사자':
+        console.log("사자")
         break;  
 }
