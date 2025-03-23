c529b75a862f9ee9348c304b936ac67d5c20f7e2
unknown <hsw_2266>
2025-03-24 01:30:16 +0900

    수정 완료

---
+++ day01/mission02.js
@@ -0,0 +1,7 @@
+let num = 1;
+let text = "test";
+let test;
+
+console.log(typeof(num));
+console.log(typeof(text));
+console.log(typeof(test));
---
+++ day01/mission04.js
@@ -0,0 +1,9 @@
+let fruits = ["Apple", "Orange", "Banana", "Grapes"];
+
+let i = 0;
+while(i < fruits.length) {
+    if (fruits[i] === "Banana"){
+        console.log("Banana가 있습니다.");
+    }
+    i = i + 1;
+}
